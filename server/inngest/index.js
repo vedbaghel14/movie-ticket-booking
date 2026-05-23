731bd9f6610f19ba94b5cf57bfe7e8df409655fa
vedbaghel14 <baghelved341@gmail.com>
2026-05-23 23:01:52 +0530

    implemented inngest and added vercel.json

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,51 @@
 const { Inngest } = require("inngest");
+const usermodel = require("../models/user.model")
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "movie-ticket-booking" });
+const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+//create a function to save user data to the database
+const syncUserCreation = inngest.createFunction(
+    {id:'sync-user-from-clerk',triggers:[{event:'clerk/user.created'}]},
+    
+async ({event}) => {
+    const {id,first_name,last_name,email_addresses,image_url} = event.data
+    const userData = {
+        _id:id,
+        email:email_addresses[0].email_address,
+        name:first_name+' '+last_name,
+        image:image_url
+    }
+    await usermodel.create(userData)
+}, )
+
+//inngest function to delete user data from the database
+const syncUserDeletion = inngest.createFunction(
+    {id:'deleted-user-with-clerk',triggers:[{event:'clerk/user.deleted'}]},
+    
+async ({event}) => {
+    const {id} = event.data
+    await usermodel.deleteOne({_id:id})}
+)
+
+//inngest function to update user data in the database
+const syncUserUpdate = inngest.createFunction(
+    {id:'update-user-with-clerk',triggers:[{event:'clerk/user.updated'}]},
+    
+async ({event}) => {
+    const {id,first_name,last_name,email_addresses,image_url} = event.data
+    const userData = {
+         _id:id,
+        email:email_addresses[0].email_address,
+        name:first_name+' '+last_name,
+        image:image_url
+}
+    await usermodel.findByIdAndUpdate(id,userData)
+
+}
+)
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+const functions = [syncUserCreation, syncUserDeletion,syncUserUpdate];
+
+module.exports = { inngest , functions };
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const {clerkMiddleware} = require('@clerk/express')
-const { serve } = require("@inngest/express")
+const { serve } = require("inngest/express")
 const { inngest, functions } = require("./inngest/index.js")
 const connectDB = require("./config/db")
 const app = express()
@@ -12,13 +12,16 @@ const port = 3000
 app.use(clerkMiddleware())
 app.use(express.json())
 app.use(cors())
-app.use("/api/inngest", serve({ client: inngest, functions }));
+
 
 connectDB()
 
 app.get('/',(req,res)=>{
     res.send("server is Live")
 })
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+
 app.listen(port, () => {
     console.log("server is running on server",port)
 })
